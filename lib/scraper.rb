45e220691cff17e4741b120ffa22c48fbdac7239
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-07 17:41:24 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -40,17 +40,17 @@ class Scraper
     tester = profile_page.css("img.social-icon").attribute("src").value
     binding.pry
     #Iterate through the student again, grabbing any links they appear and adding them to the hash
-    profile_page.css("div.social-container a").each do |link|
-      if link.css("img.social-icon").attribute("src").value == linkedin_src
-        student[:linkedin] = link.css("a")[0]["href"]
-      elsif link.css("img.social-icon").attribute("src").value == facebook_src
-        student[:facebook] = link.css("a")[0]["href"]
-      elsif link.css("img.social-icon").attribute("src").value == twitter_src
-        student[:twitter] = link.css("a")[0]["href"]
-      elsif link.css("img.social-icon").attribute("src").value == github_src
-        student[:github] = link.css("a")[0]["href"]
-      elsif link.css("img.social-icon").attribute("src").value == youtube_src
-        student[:youtube] = link.css("a")[0]["href"]
+    profile_page.css("img.social-icon").each do |link|
+      if link.attribute("src").value == linkedin_src
+        student[:linkedin] = profile_page.css("div.social-icon-container>a")[0]["href"]
+      elsif link.attribute("src").value == facebook_src
+        student[:facebook] = profile_page.css("div.social-icon-container>a")[0]["href"]
+      elsif link.attribute("src").value == twitter_src
+        student[:twitter] = profile_page.css("div.social-icon-container>a")[0]["href"]
+      elsif link.attribute("src").value == github_src
+        student[:github] = profile_page.css("div.social-icon-container>a")[0]["href"]
+      elsif link.attribute("src").value == youtube_src
+        student[:youtube] = profile_page.css("div.social-icon-container>a")[0]["href"]
       end
     end
 
