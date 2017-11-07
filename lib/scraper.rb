5ef8a221e7b2c03d90b3664b917773e380330137
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-07 17:37:16 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -37,9 +37,8 @@ class Scraper
     #Add the profile quote and the bio to the student hash of attributes
     student[:profile_quote] = profile_page.css("div.profile-quote").text
     student[:bio] = profile_page.css("div.description-holder>p").text
-    binding.pry
     profile_page.css("img.social-icon").attribute("src").value
-
+    binding.pry
     #Iterate through the student again, grabbing any links they appear and adding them to the hash
     profile_page.css("div.social-container a").each do |link|
       if link.css("img.social-icon").attribute("src").value == linkedin_src
